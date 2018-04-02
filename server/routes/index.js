940a2b38a873e18e8c72d99a8d09fb807d469641
Arion <lavrentiy.s@gmail.com>
2018-04-02 22:19:46 +0500

    Запилил тестовый вывод для сервера

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next)
+{
+    res.status(400).end();
+});
+
+router.get('/REST/events', function(req, res, next) {
+    let data = [];
+
+    let tJs = {};
+    tJs.id = 1;
+    tJs.name = "Тестовое мероприятие №1";
+    tJs.period = "10-05-2018 / 12-05-2018";
+    tJs.internal = false;
+    data.push(tJs);
+
+    tJs.id = 2;
+    tJs.name = "Тестовое мероприятие №2";
+    tJs.period = "18-06-2018 / 25-06-2018";
+    tJs.internal = true;
+    data.push(tJs);
+
+    res.send(JSON.stringify(data));
 });
 
 module.exports = router;
