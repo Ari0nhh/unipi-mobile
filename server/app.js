df9d832f3e8917ec63de0fed829db462997974ea
Arion <lavrentiy.s@gmail.com>
2018-04-06 17:44:27 +0500

    Прикрутил passport, правда не проверял еще

--- server/app.js
+++ server/app.js
@@ -1,30 +1,37 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+//var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var db = require("./model");
-
-db.sequelize.sync().then(() =>{
-    console.log("Everything looks great! :)");
-}).catch((err) =>{
-    console.log(err, "Something went wrong :(");
-});
-
+var passport = require("passport");
+var session = require("express-session");
 var indexRouter = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+
+app.use(session({
+    secret : 'old donkey ears',
+    resave : true,
+    saveUninitialized : true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+var db = require("./model");
+require("./auth/passport")(passport, db.Model.DwUser);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ server/auth/passport.js
@@ -0,0 +1,50 @@
+var bCrypt = require("bcrypt-nodejs");
+
+module.exports = function(passport, user) {
+    var User = user;
+    var LocalStrategy = require("passport-local").Strategy;
+
+    passport.use('local-signup', new LocalStrategy(
+        {
+            usernameField : 'email',
+            passwordField : 'password',
+            passReqToCallback : true
+        },
+
+        (req, email, password, done) =>
+        {
+            var generateHash = function(password, user)
+            {
+                return bCrypt.hashSync(password, User.usSalt);
+            };
+
+            User.findOne({
+                where : {usEmail: email}
+            }).then((user) =>
+            {
+                if(!user)
+                    return done(null, false);
+
+                return done(null, user);
+            })
+        }
+    ));
+
+    passport.serializeUser((user, done) =>{
+       done(null, user.usId);
+    });
+
+    passport.deserializeUser((id, done) =>
+    {
+        User.findById(id).then((user)=>
+        {
+            if(user)
+            {
+                done(null, user.get());
+            }else
+            {
+                done(user.errors, null);
+            }
+        });
+    });
+}
