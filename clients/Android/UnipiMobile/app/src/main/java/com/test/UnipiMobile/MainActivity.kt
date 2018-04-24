79c87718f9a96aade23e2a6908cb2c6784a5a02b
ilegioner <ilegioner@gmail.com>
2018-04-25 01:47:52 +0500

    CardView

--- clients/Android/UnipiMobile/app/src/main/java/com/test/UnipiMobile/MainActivity.kt
+++ clients/Android/UnipiMobile/app/src/main/java/com/test/UnipiMobile/MainActivity.kt
@@ -2,30 +2,28 @@ package com.test.UnipiMobile
 
 import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.CardView
 import android.support.v4.content.ContextCompat.startActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
-
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.Response
-
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.text.Html
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import android.widget.TextView
-
 import retrofit2.Retrofit
 import retrofit2.Callback
 
-import android.widget.Toast
 import retrofit2.http.Path
-
 
 
 class PostsAdapter(private val posts: MutableList<Event>?, val clickListener: (Event) -> Unit) : RecyclerView.Adapter<PostsAdapter.ViewHolder>() {
@@ -44,17 +42,27 @@ class PostsAdapter(private val posts: MutableList<Event>?, val clickListener: (E
 
         holder.post.text = post.evt_name //Html.fromHtml
         holder.site.text = post.evt_date_start
+        holder.site.text = post.evt_date_start
+        holder.imgStatus.setImageResource(0)
 
         holder.post.setOnClickListener{clickListener(post)}
     }
 
+//    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+//        super.onAttachedToRecyclerView(recyclerView)
+//    }
+
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var cv: CardView
         var post: TextView
         var site: TextView
+        var imgStatus: ImageView
 
         init {
+            cv = itemView.findViewById<View>(R.id.cardview_event) as CardView
             post = itemView.findViewById(R.id.postitem_post) as TextView
             site = itemView.findViewById(R.id.postitem_site) as TextView
+            imgStatus = itemView.findViewById<View>(R.id.img_status) as ImageView
         }
     }
 }
